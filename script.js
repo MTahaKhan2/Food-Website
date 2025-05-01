2f85198335f728bea003ee0e7d331644f4420332
Mohammad Taha Khan <152090420+MTahaKhan2@users.noreply.github.com>
2025-05-01 22:52:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+  const menuToggle = document.getElementById('mobile-menu');
+  const navLinks = document.getElementById('nav-links');
+
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+
